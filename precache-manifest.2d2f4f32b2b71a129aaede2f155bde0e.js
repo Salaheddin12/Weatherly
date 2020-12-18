538ddfec7e8e693ac63ed0f45f82af30a0e59b34
SalahEddine <ab.salah012@gmail.com>
2020-12-18 07:40:38 +0100

    Updates

---
+++ precache-manifest.2d2f4f32b2b71a129aaede2f155bde0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c367807bb51f8cb573d7aeb7e242a8b1",
+    "url": "/Weatherly/index.html"
+  },
+  {
+    "revision": "9744f3644a292b8a4e82",
+    "url": "/Weatherly/static/css/main.6c79fec5.chunk.css"
+  },
+  {
+    "revision": "f6f649c84fd8f08012c0",
+    "url": "/Weatherly/static/js/2.146fd9ba.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/Weatherly/static/js/2.146fd9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9744f3644a292b8a4e82",
+    "url": "/Weatherly/static/js/main.790f2a12.chunk.js"
+  },
+  {
+    "revision": "6b6f5c4f8cbe652f8772",
+    "url": "/Weatherly/static/js/runtime-main.7401531d.js"
+  }
+]);
